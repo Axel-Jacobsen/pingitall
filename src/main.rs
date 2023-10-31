abc786f233528dd0e3b75be0e62f2178b9e18e22
Axel Jacobsen <axelnj44@gmail.com>
2023-10-30 22:34:50 -0700

    refactor ping-packet creation!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use std::net::IpAddr;
 
-use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
+use pnet::util::checksum;
+
+use pnet::packet::icmp::echo_request::{EchoRequestPacket, MutableEchoRequestPacket};
 use pnet::packet::icmp::IcmpTypes;
 use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::Packet;
 
 use pnet::transport::icmp_packet_iter;
 use pnet::transport::transport_channel;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
+
+fn construct_packet(send_buffer: &mut [u8]) -> Result<EchoRequestPacket, String> {
+    let mut echo_req_packet = match MutableEchoRequestPacket::new(send_buffer) {
+        Some(packet) => packet,
+        None => return Err("failed to construct packet".to_owned()),
+    };
+
+    echo_req_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    echo_req_packet.set_sequence_number(1);
+    echo_req_packet.set_identifier(0x88F8);
+    let packet_checksum = checksum(echo_req_packet.packet(), 1);
+    echo_req_packet.set_checksum(packet_checksum);
+
+    Ok(echo_req_packet.consume_to_immutable())
+}
 
 #[allow(unused_variables)]
 fn main() {
@@ -18,30 +36,29 @@ fn main() {
         transport_channel(4096, protocol).expect("failed to make transport channel");
 
     let mut send_buffer = [0u8; 64];
-    let mut echo_req_packet = MutableEchoRequestPacket::new(&mut send_buffer).unwrap();
-
-    echo_req_packet.set_icmp_type(IcmpTypes::EchoRequest);
-    echo_req_packet.set_sequence_number(1);
-    echo_req_packet.set_identifier(0xCAFE);
-    echo_req_packet.set_checksum(0x2d00); // from wireshark lol
+    let echo_req_packet = match construct_packet(send_buffer.as_mut()) {
+        Ok(packet) => packet,
+        Err(e) => panic!(
+            "Failed to construct packet, most likely because of too small of a send buffer: {:?}",
+            e
+        ),
+    };
 
     tx.send_to(echo_req_packet, target_ip)
         .expect("Failed to send packet");
 
     let mut icmp_iter = icmp_packet_iter(&mut rx);
-    loop {
-        match icmp_iter.next() {
-            Ok((packet, addr)) => match packet.get_icmp_type() {
-                IcmpTypes::EchoReply => {
-                    println!("Received Echo Reply");
-                }
-                _ => {
-                    println!("Received unexpected packet");
-                }
-            },
-            Err(e) => {
-                println!("Failed to receive packet: {:?}", e);
+    match icmp_iter.next() {
+        Ok((packet, addr)) => match packet.get_icmp_type() {
+            IcmpTypes::EchoReply => {
+                println!("Received Echo Reply");
             }
+            _ => {
+                println!("Received unexpected packet");
+            }
+        },
+        Err(e) => {
+            println!("Failed to receive packet: {:?}", e);
         }
     }
 }
